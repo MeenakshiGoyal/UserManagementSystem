721cb7b8266ff81c7ae39da478db639f1f203238
MeenakshiGoyal <76804533+MeenakshiGoyal@users.noreply.github.com>
2021-03-30 11:34:05 +0530

    Delete User Management System/src/main/scala/org/knoldus directory

--- User Management System/src/main/scala/org/knoldus/models/User.scala
+++
@@ -1,5 +0,0 @@
-package org.knoldus.models
-
-import java.util.UUID
-
-case class User(id: Option[UUID], name:String , age:Int ,userRole:Value)
--- User Management System/src/main/scala/org/knoldus/models/Value.scala
+++
@@ -1,8 +0,0 @@
-package org.knoldus.models
-sealed trait  Value
-
-object Value{
-  case object Admin extends Value
-
-  case object Customer extends Value
-}
